a0053598b2ae6c57af54003aa7b5c17075e549f8
SeanTangney <sean_tangney@hotmail.com>
2022-01-23 18:07:03 +0000

    Add game instructions and game feedback

--- run.py
+++ run.py
@@ -18,7 +18,11 @@ def print_board(board):
         print(" ".join(row))
 
 
-print("Let's Play")
+print("\n-=-=-BATTLESHIP-=-=-\n")
+print("How to play:")
+print("* A battleship has been randomly placed on the board.")
+print("* You have four tries to find it! ")
+print("* Enter your coordinates (from 1-5) below\nBest of Luck!!\n")
 print_board(board)
 
 
@@ -26,33 +30,48 @@ def random_row(board):
     """
     A function to generate a random row selection
     """
-    return randint(0, len(board[0]) - 1)
+    return randint(1, len(board) + 1)
 
 
 def random_col(board):
     """
     A function to generate a random column selection
     """
-    return randint(0, len(board[0]) - 1)
+    return randint(1, len(board) + 1)
 
 
 ship_row = random_row(board)
 ship_col = random_col(board)
 
 for guess in range(4):
-    guess_row = int(input("Guess Row: "))
-    guess_col = int(input("Guess Column: "))
+    guess_row = int(input("\nGuess Row: "))
+    guess_col = int(input("\nGuess Column: "))
 
     if guess_row == ship_row and guess_col == ship_col:
+        print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-")
         print("You've sunk the battleship")
+        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
         break
     else:
-        if(guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        if(guess_row < 1 or guess_row > 5) or (guess_col < 1 or guess_col > 5):
+            print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
             print("Your guess is outside the board")
+            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
 
-        elif board[guess_row][guess_col] == "X":
+        elif board[guess_row][guess_col] == " X":
             print("You've already guessed that one!")
 
         else:
+            print("\n-=-=-")
             print("Miss!")
-            board[guess_row][guess_col] = "X"
+            print("-=-=-\n")
+            board[guess_row][guess_col] = " X"
+
+        print("Guess " + str(guess + 1) + " out of 5.\n")
+        print_board(board)
+
+    if guess >= 5:
+        print("\n-=-=-=-=-=-")
+        print("Game over!")
+        print("-=-=-=-=-=-\n")
+
