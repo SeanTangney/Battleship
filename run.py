17e23d2e7e738185caa76c38391f63f3c350752d
SeanTangney <sean_tangney@hotmail.com>
2022-01-23 13:47:02 +0000

    Define print_board function

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+board = []
+
+for (x) in range(5):
+    board.append([" O"] * 5)
+
+def print_board(board):
+    """
+    Print out game board in the terminal
+    """
+    for row in board:
+        print(" ".join(row))
