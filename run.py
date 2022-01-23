1d60c8a2672f7f0d605819dd05369c007ca2bacf
SeanTangney <sean_tangney@hotmail.com>
2022-01-23 16:14:37 +0000

    Added functions to generate row and col coordinates

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
+from random import randint
+
 
 board = []
 
 for (x) in range(5):
     board.append([" O"] * 5)
+
 
 def print_board(board):
     """
@@ -10,3 +13,20 @@ def print_board(board):
     """
     for row in board:
         print(" ".join(row))
+
+
+print("Let's Play")
+print_board(board)
+
+
+def random_row(board):
+    """
+    A function to generate a random row selection
+    """
+    return randint(0, len(board[0]) -1)
+
+def random_col(board):
+    """
+    A function to generate a random column selection
+    """
+    return randint(0, len(board[0]) -1)
