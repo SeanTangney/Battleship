df6970f709908cf620a7485fa5644a0f9eaa29ad
SeanTangney <sean_tangney@hotmail.com>
2022-01-24 20:03:36 +0000

    Add more validation to user inputs

--- run.py
+++ run.py
@@ -31,48 +31,50 @@ def random_row(board):
     """
     A function to generate a random row selection
     """
-    return randint(0, len(board) + 1)
+    return randint(0, len(board))
 
 
 def random_col(board):
     """
     A function to generate a random column selection
     """
-    return randint(0, len(board) + 1)
+    return randint(0, len(board))
 
 
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-for guess in range(6):
-    guess_row = int(input("\nGuess Row: "))
-    guess_col = int(input("\nGuess Column: "))
+guess = 0
+for guess in range(5):
+    while(True):
+        try:
+            guess_row = int(input("\nGuess Row: "))
+            guess_col = int(input("\nGuess Column: "))
+            
+            if guess_row == ship_row and guess_col == ship_col:
+                print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+                print(" You've sunk the battleship")
+                print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+                print("          You Win!")
+                print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+                break
+            else:
+                if(guess_row < 1 or guess_row > 5) or (guess_col < 1 or guess_col > 5):
+                    print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+                    print("Your guess is outside the board")
+                    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-        print(" You've sunk the battleship")
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
-        print("          You Win!")
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
-        break
-    else:
-        if(guess_row <= 0 or guess_row >= 6) or (guess_col <= 0 or guess_col >= 6):
-            print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-            print("Your guess is outside the board")
-            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+                elif(board[guess_row - 1][guess_col - 1] == " X"):
+                    print("You've already guessed that one!")
 
-        elif board[guess_row][guess_col] == " X":
-            print("You've already guessed that one!")
-
-        else:
-            print("\n-=-=-")
-            print("Miss!")
-            print("-=-=-\n")
-            board[guess_row - 1][guess_col - 1] = " X"
-        if(guess == 5):
-            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
-            print("          You Win!")
-            print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
-        else:
-            print("Guess " + str(guess + 1) + " out of 5.\n")
-            print_board(board)
+                else:
+                    print("\n-=-=-")
+                    print("Miss!")
+                    print("-=-=-\n")
+                    board[guess_row - 1][guess_col - 1] = " X"
+                    print("Guess " + str(guess + 1) + " out of 5.\n")
+                    if guess == 5:
+                    print_board("Game Over")
+        except:
+            print("Invalid entry")
+        
