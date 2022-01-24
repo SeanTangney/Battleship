7924709d95bbedf9fa3472cf4fd73464ca1f90c3
SeanTangney <sean_tangney@hotmail.com>
2022-01-24 20:46:43 +0000

    Add final indenting and error checks

--- run.py
+++ run.py
@@ -23,7 +23,8 @@ print("How to play:")
 print("* A battleship has been randomly placed on the board.")
 print("* The objective is to target it with your coordinates to hit it.")
 print("* You have four tries to find it! ")
-print("* Enter your coordinates (from 1-5) below\nBest of Luck!!\n")
+print("* Previous missed attempts are displayed on the board with an X ")
+print("* Enter your coordinates (from 1-5) below\n  Best of Luck!!\n")
 print_board(board)
 
 
@@ -44,22 +45,24 @@ def random_col(board):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-guess = 0
+shot = 0
 for guess in range(5):
     while(True):
         try:
             guess_row = int(input("\nGuess Row: "))
             guess_col = int(input("\nGuess Column: "))
-            
+
             if guess_row == ship_row and guess_col == ship_col:
                 print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-")
                 print(" You've sunk the battleship")
                 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
-                print("          You Win!")
+                print("          You Win!\n")
                 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+
                 break
             else:
-                if(guess_row < 1 or guess_row > 5) or (guess_col < 1 or guess_col > 5):
+                if((guess_row < 1 or guess_row > 5) or
+                        (guess_col < 1 or guess_col > 5)):
                     print("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
                     print("Your guess is outside the board")
                     print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
@@ -71,10 +74,15 @@ for guess in range(5):
                     print("\n-=-=-")
                     print("Miss!")
                     print("-=-=-\n")
+                    shot += 1
                     board[guess_row - 1][guess_col - 1] = " X"
-                    print("Guess " + str(guess + 1) + " out of 5.\n")
-                    if guess == 5:
-                    print_board("Game Over")
+                    print("Guess " + str(shot) + " out of 5.\n")
+                    print_board(board)
+                    if shot == 5:
+                        print("\n-=-=-=-=-=-=-=-=-=-=-")
+                        print("\n     Game Over")
+                        print("\nBetter Luck Next Time")
+                        print("\n-=-=-=-=-=-=-=-=-=-=-")
+                    break
         except:
             print("Invalid entry")
-        
